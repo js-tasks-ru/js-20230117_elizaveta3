ebebff60fae653427f2c1c1bb6c5fe7e0858f5bf
LisaB1994 <79035700491@yandex.ru>
2023-01-17 22:04:46 +0300

    do-homework-1

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -1,10 +1,2 @@
-/**
- * Sum of two numbers
- *
- * @param {number} m first number
- * @param {number} n second number
- * @returns {number}
- */
-export const sum = (m, n) => {
+export const sum = (m, n) => m + n;
 
-};
